0561bd130da26141c8243bd16806991e80921fd3
FilippaKD <fd222hz@student.lnu.se>
2025-02-23 13:44:43 +0100

    offline

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ import { initClicker } from "./js/utils.js";
 
 function init() {
   initClicker("btn", "output");
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+        console.log("Service worker registration succeeded:", registration);
+      },
+      (error) => {
+        console.error(`Service worker registration failed: ${error}`);
+      },
+    );
+  }
+
+
+
+
 }
 window.addEventListener("load", init);
